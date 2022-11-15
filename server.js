a582a4e0fe69e09ddbcc7628f348d6901c1c010e
SemihB <semihr5171@gmail.com>
2022-11-14 23:27:56 -0500

    show route

---
+++ server.js
@@ -0,0 +1,47 @@
+//------------------------------
+// Dependencies
+//------------------------------
+require('dotenv').config(); // read the .env file (process.env.PORT)
+const express = require('express'); // backend framework
+const morgan = require("morgan") // logger
+const methodOverride = require("method-override")
+const budgets = require("./models/budget.js");
+//-------------------------------
+// Global Variables
+//-------------------------------
+const PORT = process.env.PORT || 3000
+
+//-----------------------------
+// Create Application Object
+//-----------------------------
+const app = express(); // create our application object
+
+//-----------------------------
+// Middleware
+//-----------------------------
+app.use(morgan("tiny"));
+app.use(express.urlencoded({extended: true}))
+
+//-----------------------------
+// Routes
+//-----------------------------
+app.get("/", (req, res) => res.redirect('/budgets') )
+
+
+app.get("/budgets", (req, res) => {
+    res.render('index.ejs', {
+        allBudget: budgets
+    });
+});
+
+app.get('/budgets/:index', (req, res) => {
+    res.render('show.ejs', {
+        budget: budgets[req.params.index],
+        index: req.params.index
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+});
